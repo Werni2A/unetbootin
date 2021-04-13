311b313f4c71edbb4e20d2c5e5ab56ebe66a4f3b
Dominik Wernberger <dominik.wernberger@gmx.de>
2021-04-13 23:41:58 +0200

    asdfadf

--- src/unetbootin/customrelease.sh
+++ src/unetbootin/customrelease.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 lupdate customdistrolst.cpp customdistselect.cpp -ts unetbootin_custom.ts
 lrelease unetbootin_custom.ts -qm unetbootin_custom.qm
--- src/unetbootin/mkcustomsfx.sh
+++ src/unetbootin/mkcustomsfx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 7z a unetbootin.7z unetbootin.exe unetbootin_custom.qm
 cat 7zS.sfx config.txt unetbootin.7z > unetbootin-customsfx-windows.exe
